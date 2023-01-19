13616722d9c3fa5d8b764fe49ab2e962eec368be
Harrison Gordon <19hmkg@queensu.ca>
2023-01-19 03:12:02 -0500

    unit testing skeleton code

--- Real_Time_Clock/test_rtc.py
+++ Real_Time_Clock/test_rtc.py
@@ -1,6 +1,13 @@
 import rtc_raspberri_pi as rtc
 import time
 
+
+def test_date_time(raw,expected):
+    pass
+def test_check_tick(raw,expected):
+    pass
+def test_encode(raw,expected):
+    pass
 my_rtc = rtc.RTC(1,1)
 
 my_rtc.year = 22
