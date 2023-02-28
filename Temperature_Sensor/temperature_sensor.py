4f88d5efba04835eabb74980b886fa22844197d2
Harrison Gordon <19hmkg@queensu.ca>
2023-02-28 18:43:03 -0500

    misc changes

--- Temperature_Sensor/temperature_sensor.py
+++ Temperature_Sensor/temperature_sensor.py
@@ -1,7 +1,6 @@
-import time
 from   smbus import SMBus
 
-class TEMP_SENSOR:
+class Temperature_Sensor:
     '''
     TEMP class
     I2C interface class written for MCP9808
@@ -27,11 +26,7 @@ class TEMP_SENSOR:
 
     def __init__(self, i2c_status, crit_temperature = None, upper_temperature = None, lower_temperature = None):
         '''
-        Initialization of RTC class
-        
-        Args:
-            battery_state: 0 = backup battery off, 1 = backup battery on
-            clock_state: 0 = clock off, 1 = clock on
+        Initialization of Temperature_Sensor class
         '''
         if crit_temperature:
             self.set_temperature()
@@ -61,46 +56,15 @@ class TEMP_SENSOR:
                return -1
         return 0   
     
-    def __decode_ambient(self,value):
-        '''
-        Decodes register value from Temperature Sensor ambient
-        temperature register to a user readable value.  
-        
-        Returns:
-           Decoded ambient temperature value 
-        '''
-        ones = value % 10
-        tens = int(value / 10)
-        return (tens<<4 | ones)
-
-    def get_ambient_temp(self):
-        '''
-        Decodes register value from temperature Sensor ambient
-        temperature register to a user readable value.  
-        
-        Returns:
-           Decoded ambient temperature value 
-        '''
-        temp_reg_data = self.i2c_bus.read_byte_data(self.registers['slave'],self.registers['t_ambient'])
-        sign_bit =   temp_reg_data &  (0b1 << 12)
-        temp_reg_data =   temp_reg_data &  0b1111,1111,1111
-        power = -4
-        converted_temp = 0
-        while(temp_reg_data != 0):
-            converted_temp =+ 2 ** power
-            power =+ 1
-            temp_reg_data = temp_reg_data>>1 
-        if sign_bit > 0:
-            return converted_temp*-1
-        else:
-            return converted_temp
-
     def set_temperature(self, register, value):
         '''
         Return the decoded value of a temperature from a given register
         '''
-        if value < 0:
-
+        pass
+        # sign_bit = 0
+        # if value < 0:
+        #     sign_bit = 1
+        #     value * -1
 
     def get_temperature(self, register):
         '''
@@ -119,26 +83,3 @@ class TEMP_SENSOR:
     @property
     def ambient(self):
         return self.get_temperature(self.registers['t_ambient'])
-
-    def get_ambient_temp(self):
-        '''
-        Decodes register value from temperature Sensor ambient
-        temperature register to a user readable value.  
-        
-        Returns:
-           Decoded ambient temperature value 
-        '''
-        temp_reg_data = self.i2c_bus.read_i2c_block_data(self.registers['slave'],self.registers['t_ambient'], 2)
-        upper_byte =  temp_reg_data[0] & 0x1F
-        lower_byte = temp_reg_data[1]/16
-        if upper_byte & 0x10:
-            upper_byte = (upper_byte & 0x0F)*16
-            return 256 - (upper_byte + lower_byte)
-        else: 
-            upper_byte = upper_byte *16
-            return upper_byte + lower_byte
-
-temp_sensor = TEMP_SENSOR()
-while 1:
-    time.sleep(1)
-    print(f"Current Temperature is :{temp_sensor.ambient}") 
