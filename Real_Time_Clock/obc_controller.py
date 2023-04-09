6820743100027f2894937cf77ccb5c8463f6ce3c
Harrison Gordon <19hmkg@queensu.ca>
2023-04-09 18:19:50 -0400

    new obc controller branch

---
+++ Real_Time_Clock/obc_controller.py
@@ -0,0 +1,9 @@
+class OBC_Contoller:
+    """This is a class to contain various fucntions and operations of the on board controller.
+        
+        Functionality:
+        1) Get telemetry
+    """
+    def get_telemetry():
+        pass
+
