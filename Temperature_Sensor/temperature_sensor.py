de4fb50fcdda004b312266cef61a7b96b8d63c0a
Harrison Gordon <19hmkg@queensu.ca>
2023-04-15 05:48:39 +0100

    impelement setting functionality for upper lower and critical temperatures

--- Temperature_Sensor/temperature_sensor.py
+++ Temperature_Sensor/temperature_sensor.py
@@ -1,4 +1,4 @@
-from   smbus import SMBus
+from smbus import SMBus
 
 class Temperature_Sensor:
     '''
@@ -7,8 +7,10 @@ class Temperature_Sensor:
     Data Sheet: https://ww1.microchip.com/downloads/en/DeviceDoc/25095A.pdf
 
     Functionality:
+    - Get Ambient Temperature
     - Set/Get Critical Temperature
-    - Get Temperature
+    - Set/Get Upper Temperature
+    - Set/Get Lower Temperature
     '''
     i2c_bus = SMBus(1)
 
@@ -65,18 +67,26 @@ class Temperature_Sensor:
 
     def set_temperature(self, register, value):
         '''
-        Return the decoded value of a temperature from a given register
+        Return the decoded value of a temperature from a given register. Supported for values up until 256 
         '''
-        upper_byte = int(value/ 16) >> 8 #isolate Upper bits
-        lower_byte = (value * 16)
-
         if value < 0:
+
+            value = int(value * -16)
+            upper_byte = value >> 8 #isolate Upper bits
+            lower_byte = value & 0xFF
+
             for i in range(4):
                 upper_byte = self.bit_inverse(i,upper_byte)
             for i in range(8):
                 lower_byte = self.bit_inverse(i,lower_byte)
+
             upper_byte |= 0x10 # add sign bit
-        pass
+
+        else:
+            value = int(value * 16)
+            upper_byte = value >> 8 #isolate upper bits
+            lower_byte = value & 0xFF
+
         self.i2c_bus.write_i2c_block_data(self.registers['slave'],register, [upper_byte,lower_byte])
 
     def get_temperature(self, register):
@@ -87,15 +97,19 @@ class Temperature_Sensor:
         upper_byte =  temp_reg_data[0] & 0x1F
         lower_byte = (temp_reg_data[1]) / 16
         if upper_byte & 0x10:
-            upper_byte = (upper_byte & 0x0F) * 16
-            return 256 - (upper_byte + lower_byte)
+            upper_byte = (upper_byte & 0xF) * 16
+            return -255.75 + (upper_byte + lower_byte)
         else: 
             upper_byte = upper_byte * 16
             return upper_byte + lower_byte
 
     @property
-    def critcal_temp(self):
+    def critical_temp(self):
         return self.get_temperature(self.registers['t_crit'])
+
+    @critical_temp.setter
+    def critical_temp(self,value):
+        self.set_temperature(self.registers['t_crit'],value)
 
     @property
     def upper_temp(self):
@@ -108,6 +122,10 @@ class Temperature_Sensor:
     @property
     def lower_temp(self):
         return self.get_temperature(self.registers['t_lower'])
+
+    @lower_temp.setter
+    def lower_temp(self,value):
+        self.set_temperature(self.registers['t_lower'],value)
 
     @property
     def ambient(self):
