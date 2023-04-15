0951b2c005629e9563152c4f7bbacbcc0fb2cc68
Harrison Gordon <19hmkg@queensu.ca>
2023-04-16 00:59:45 +0100

    bring telemetry function to a working state and add parsing capabilites

--- Real_Time_Clock/obc_controller.py
+++ Real_Time_Clock/obc_controller.py
@@ -1,33 +1,55 @@
-import Yaml
-import RTC 
-import Temperature_Sensor
+import yaml
+from rtc import RTC 
+from temperature_sensor import Temperature_Sensor
 import argparse
 
-class OBC_Contoller:
+class OBC_Controller:
     """This is a class to contain various fucntions and operations of the on board controller. This class is meant to be used through a CLI.
         
         Functionality:
             1) Yaml file configurability 
             2) Aquire telemetry from multiple devices
     """
+    config_path =  "controller_config.yml"
+
     @staticmethod
     def get_config():
-        pass
+        with open(OBC_Controller.config_path, 'r') as file:
+            return yaml.safe_load(file)
 
     @staticmethod
     def init_rtc():
-        config = get_config()
+        config = OBC_Controller.get_config()
         rtc_config = config["rtc"]
         return RTC(rtc_config['battery_state'],rtc_config['clock_state'],rtc_config['i2c_status'])
 
     @staticmethod
     def init_temp():
-        config = get_config()
+        config = OBC_Controller.get_config()
         temp_config = config["temperature_sensor"]
-        return Temperature_Sensor(temp_config["i2c_status"],temp_config["critical_temp"],temp_config["upper_temperature",temp_config["lower_temperature"])
-
+        return Temperature_Sensor(temp_config["i2c_status"])
+        #return Temperature_Sensor(temp_config["i2c_status"],temp_config["critical_temperature"],temp_config["upper_temperature"],temp_config["lower_temperature"])
 
     @staticmethod
-    def get_telemetry(verbose):
-        temp_sense_contoller = init_temp()
-        rtc_controller = init_rtc() 
+    def get_telemetry():
+        temp_interface = OBC_Controller.init_temp()
+        rtc_interface = OBC_Controller.init_rtc() 
+
+        print(f"Time: {rtc_interface.datetime}")
+        print(f"OBC Ambient Temperature: {temp_interface.ambient} °C")
+
+
+    def test():
+        print("hello")
+
+if __name__  == '__main__':
+    FUNCTION_MAP =  {
+        'telemetry': OBC_Controller.get_telemetry,
+        'test' : OBC_Controller.test,
+        }
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cmd', choices=FUNCTION_MAP.keys())
+    args = parser.parse_args()
+
+    func = FUNCTION_MAP[args.cmd]
+    func()
