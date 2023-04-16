2a0155c5b65620d71e3e088f3ef359242ee37ca1
Harrison Gordon <60119461+HarrisonMKG@users.noreply.github.com>
2023-04-15 22:34:41 -0400

    Clean usage.py

--- Real_Time_Clock/usage.py
+++ Real_Time_Clock/usage.py
@@ -1,8 +1,8 @@
 import time
 from rtc import RTC
 
-my_rtc = RTC(1,1,0) # Initalize RTC
-my_rtc.datetime = "2021-2-5-0-15-34" # Set Time
+my_rtc = RTC(1,1,0,"2021-2-5-0-15-34") # Initalize RTC
+
 while 1:
     time.sleep(1)
     print(f"{my_rtc.datetime}  |  [Year,Month,Day,Hour,Minute,Second]")
